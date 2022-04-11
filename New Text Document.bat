d97fc5fe849542e76cae2c1d75574b31fb43723e
Benyamin-Vala <102605148+Benyamin-Vala@users.noreply.github.com>
2022-04-11 17:18:21 +0300

    Delete New Text Document.bat

--- New Text Document.bat
+++
@@ -1,7 +0,0 @@
-echo "# Contact-Book" >> README.md
-git init
-git add .
-git commit -m "Upload Contact Book Project on Github"
-git branch -M main
-git remote add origin https://github.com/Benyamin-Vala/Contact-Book.git
-git push -u origin main
